89f088bac59c42be46f3d0e4aa5c7cf9e7ff9fbc
PhilippeHenry <pipe.henry@gmail.com>
2022-07-06 15:21:23 +0200

    Battery works

--- dotfiles/.scripts/batterycheck.sh
+++ dotfiles/.scripts/batterycheck.sh
@@ -3,17 +3,17 @@
 # low battery warning
 #
 
-THRESHOLD=15
-BATTERY=/sys/class/power_supply/BAT0
-CHARGING=$(cat /sys/class/power_supply/AC/online)
+THRESHOLD=20
+BATTERY=/sys/class/power_supply/BAT1
+CHARGING=$(cat /sys/class/power_supply/BAT1/online)
 
 if [[ CHARGING -eq "1" ]]; then
     echo "Charging, stopping early..."
     exit 0
 fi
 
-REM=`grep "POWER_SUPPLY_CHARGE_NOW" $BATTERY/uevent | awk -F= '{ print $2 }'`
-FULL=`grep "POWER_SUPPLY_CHARGE_FULL_DESIGN" $BATTERY/uevent | awk -F= '{ print $2 }'`
+REM=`grep "POWER_SUPPLY_ENERGY_NOW" $BATTERY/uevent | awk -F= '{ print $2 }'`
+FULL=`grep "POWER_SUPLY_ENERGY_FULL_DESIGN" $BATTERY/uevent | awk -F= '{ print $2 }'`
 PERCENT=`echo $(( $REM * 100 / $FULL ))`
 
 if [ $PERCENT -le "$THRESHOLD" ]; then
